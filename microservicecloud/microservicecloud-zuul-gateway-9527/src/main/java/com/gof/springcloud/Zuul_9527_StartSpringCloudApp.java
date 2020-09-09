21546c58781cb828719b2e5738df0384f90b0f61
timothy <timhou@outlook.com>
2020-09-09 22:25:50 +0800

    add cors filter to zuul

--- microservicecloud/microservicecloud-zuul-gateway-9527/src/main/java/com/gof/springcloud/Zuul_9527_StartSpringCloudApp.java
+++ microservicecloud/microservicecloud-zuul-gateway-9527/src/main/java/com/gof/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -3,6 +3,11 @@ package com.gof.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +17,23 @@ public class Zuul_9527_StartSpringCloudApp
 	{
 		SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
 	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
+
